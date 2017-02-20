be3c77c60ce519dca924df4c10390260a1863d11
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2017-02-19 21:58:51 -0300

    1
    1

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'MFH POS',
+    'author': 'Marlon Falcón Hernández',
+    'category': 'Sales',
+    'description': """
+Ejemplo de módulo
+=====================================================
+        """,
+    'version': '10.0.0.1',
+    'depends': ['point_of_sale'],
+    'data': [ 'views/template.xml'
+    ],
+    'qweb': ['static/src/xml/pos.xml'],
+    'demo': [],
+    'auto_install': False,
+}
---
+++ static/src/js/mfh_pos.js
@@ -0,0 +1,12 @@
+odoo.define('mfh_pos.point_of_sale', function (require) {
+"use strict";
+    var Model = require('web.DataModel');
+    var ActionManager1 = require('web.ActionManager');
+    var core = require('web.core');
+    var models = require('point_of_sale.models');
+    var _t = core._t;
+    var PosBaseWidget = require('point_of_sale.BaseWidget');
+    models.load_fields('res.company','street');
+    models.load_fields('res.company','city');
+    models.load_fields('res.company','zip');
+});
